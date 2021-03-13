8b66d2cdd271592064c8172cb440af6d0c1322d9
Andre de Lima e Silva <maveco69@gmail.com>
2021-03-13 17:50:37 -0300

    Addded statuspage tests

---
+++ src/statuspage.test.ts
@@ -0,0 +1,32 @@
+import DefaultOperations from './defaultOperations';
+import StatusPage from './statuspage';
+
+const pageId = "12345";
+const apiKey = "##key##";
+
+test('I can import StatusPage', () => {
+    expect(StatusPage).toBeDefined();
+});
+
+test('I can require StatusPage', () => {
+    expect(require('./StatusPage')).toBeDefined();
+});
+
+test("verify components", async () => {
+  const path = "components";
+  const statusPage = new StatusPage(pageId, apiKey);
+  expect(statusPage.components()).toStrictEqual(new DefaultOperations<Object>(pageId, apiKey, path));
+});
+
+test("verify component-groups", async () => {
+    const path = "component-groups";
+    const statusPage = new StatusPage(pageId, apiKey);
+    expect(statusPage.componentGroups()).toStrictEqual(new DefaultOperations<Object>(pageId, apiKey, path));
+  });
+
+  test("verify incidents", async () => {
+    const path = "incidents";
+    const statusPage = new StatusPage(pageId, apiKey);
+    expect(statusPage.incidents()).toStrictEqual(new DefaultOperations<Object>(pageId, apiKey, path));
+  });
+
